da1b296b4610bd4f904fe4e5f28dd3da3e34df3f
Martin Rieser <martin.h.rieser@gmail.cm>
2026-07-11 17:15:08 +0200

    Fix NPE in DataContainerGlobal when _data is null after clear(); upgrade GitHub Actions to v5/v7

--- src/main/java/ch/technokrat/gecko/geckocircuits/datacontainer/DataContainerGlobal.java
+++ src/main/java/ch/technokrat/gecko/geckocircuits/datacontainer/DataContainerGlobal.java
@@ -128,6 +128,9 @@ public final class DataContainerGlobal extends AbstractDataContainer implements 
 
   @Override
   public ContainerStatus getContainerStatus(){
+    if(_data == null){
+      return ContainerStatus.FINISHED;
+    }
     return _data.getContainerStatus();
   }
 
@@ -143,6 +146,9 @@ public final class DataContainerGlobal extends AbstractDataContainer implements 
 
   @Override
   public void update(final Observable observable, final Object arg){
+    if(_data == null){
+      return;
+    }
 
     if(getMaximumTimeIndex(0) != _lastDataIndex){
       this.setChanged();
