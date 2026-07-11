42fc0bb6caf8cb82d3204c7ffd4eebc51f14d06f
Martin Rieser <martin.h.rieser@gmail.cm>
2026-07-11 16:55:21 +0200

    Fix deprecation warnings: use non-deprecated HiLoData from newscope package in test

--- src/test/java/ch/technokrat/gecko/geckocircuits/scope/HiLoDataTest.java
+++ src/test/java/ch/technokrat/gecko/geckocircuits/scope/HiLoDataTest.java
@@ -1,5 +1,6 @@
 package ch.technokrat.gecko.geckocircuits.scope;
 
+import ch.technokrat.gecko.geckocircuits.newscope.HiLoData;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -7,31 +8,26 @@ public class HiLoDataTest {
 
     @Test
     public void testInsertCompareValue() {
-        HiLoData data = new HiLoData();
-        assertEquals(1E30f, data.yLo, 1e-9);
-        assertEquals(-1E30f, data.yHi, 1e-9);
+        HiLoData data = null;
 
-        data.insertCompare(5.5f);
-        assertEquals(5.5f, data.yLo, 1e-6f);
-        assertEquals(5.5f, data.yHi, 1e-6f);
+        data = HiLoData.mergeFromValue(data, 5.5f);
+        assertEquals(5.5f, data._yLo, 1e-6f);
+        assertEquals(5.5f, data._yHi, 1e-6f);
 
-        data.insertCompare(-3.2f);
-        data.insertCompare(10.0f);
-        assertEquals(-3.2f, data.yLo, 1e-6f);
-        assertEquals(10.0f, data.yHi, 1e-6f);
+        data = HiLoData.mergeFromValue(data, -3.2f);
+        data = HiLoData.mergeFromValue(data, 10.0f);
+        assertEquals(-3.2f, data._yLo, 1e-6f);
+        assertEquals(10.0f, data._yHi, 1e-6f);
     }
 
     @Test
     public void testInsertCompareData() {
-        HiLoData d1 = new HiLoData();
-        d1.insertCompare(2.0f);
+        HiLoData d1 = HiLoData.mergeFromValue(null, 2.0f);
+        HiLoData d2 = HiLoData.mergeFromValue(null, -1.0f);
+        d2 = HiLoData.mergeFromValue(d2, 5.0f);
 
-        HiLoData d2 = new HiLoData();
-        d2.insertCompare(-1.0f);
-        d2.insertCompare(5.0f);
-
-        d1.insertCompare(d2);
-        assertEquals(-1.0f, d1.yLo, 1e-6f);
-        assertEquals(5.0f, d1.yHi, 1e-6f);
+        HiLoData merged = HiLoData.merge(d1, d2);
+        assertEquals(-1.0f, merged._yLo, 1e-6f);
+        assertEquals(5.0f, merged._yHi, 1e-6f);
     }
 }
